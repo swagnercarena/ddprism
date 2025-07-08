4b5175fa82a1b4608e839ce7f3cae44bc8217b89
Aizhan Akhmetzhanova <aakhmetzhanova@workergpu023.cm.cluster>
2025-07-08 16:45:42 -0400

    add config file for pcpca

---
+++ ddprism/rand_manifolds/scaling_n_models/config_base_pcpca.py
@@ -0,0 +1,25 @@
+"""Basic configuration for the contrastive problem with Gibbs sampling."""
+import config_base
+from ml_collections import ConfigDict
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+    config = config_base.get_config()
+    
+    # Parameters for PCPCA
+    config.gamma = [0., 0.3, 0.3]
+    config.latent_dim = 5
+
+    # Optimization hyperparameters
+    config.n_iter = 4_000
+    config.learning_rate = 1e-2
+    config.lr_schedule = 'linear'
+
+    config.log_figure = True
+
+    # wandb parameters
+    config.wandb_kwargs = ConfigDict(
+        {'project': 'pcpca_rand_manifolds', 'mode': 'online', 'run_name': None}
+            )
+    
+    return config

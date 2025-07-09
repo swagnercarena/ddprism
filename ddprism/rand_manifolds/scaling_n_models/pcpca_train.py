c5238492cbb441f408aaef9d7f8a88ee63edd58f
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2025-07-09 17:06:06 -0400

    updated training script.

--- ddprism/rand_manifolds/scaling_n_models/pcpca_train.py
+++ ddprism/rand_manifolds/scaling_n_models/pcpca_train.py
@@ -65,19 +65,19 @@ def main(_):
         # Get the gamma and relevant observations.
         gamma = gamma_list[source_index]
 
-        # In PCPCA language, x_obs is enriched signal and y_obs is background.
-        x_obs = y_all[:, source_index]
+        # In PCPCA language, y_enr is enriched observation and y_bkg is background.
+        y_enr = y_all[:, source_index]
 
         if source_index == 0:
-            y_obs = jnp.zeros_like(x_obs)
+            y_bkg = jnp.zeros_like(y_enr)
         else:
-            y_obs = y_all[:, source_index-1]
+            y_bkg = y_all[:, source_index-1]
 
-        x_a_mat = A_all[:,source_index]
+        enr_a_mat = A_all[:,source_index]
         if source_index == 0:
-            y_a_mat = jnp.zeros_like(x_a_mat)
+            bkg_a_mat = jnp.zeros_like(enr_a_mat)
         else:
-            y_a_mat = A_all[:,source_index-1]
+            bkg_a_mat = A_all[:,source_index-1]
 
         # Initialize W and log_sigma.
         rng_w, rng = jax.random.split(rng, 2)
@@ -105,46 +105,62 @@ def main(_):
             )
 
         # Run the optimization loop.
-        for iter in range(config.n_iter):
+        for step in range(config.n_iter):
             grad = pcpca_utils.loss_grad(
-                params, x_obs, y_obs, x_a_mat, y_a_mat, gamma
+                params, y_enr, y_bkg, enr_a_mat, bkg_a_mat, gamma
             )
             loss = pcpca_utils.loss(
-                params, x_obs, y_obs, x_a_mat, y_a_mat, gamma
+                params, y_enr, y_bkg, enr_a_mat, bkg_a_mat, gamma
             )
-            params['weights'] -= schedule(iter) * grad['weights']
+            params['weights'] -= schedule(step) * grad['weights']
 
             # Log our loss
-            wandb.log({f'loss {source_index + 1}': loss}, step=iter)
+            wandb.log({f'loss {source_index + 1}': loss}, step=step)
 
-        ### CODE BELOW IS FOR FUTURE VERIFICATION.
+        # Get the posterior for the signal underlying the observation.
+        x_mean, x_cov = jax.vmap(
+            pcpca_utils.calculate_posterior, in_axes=(None, 0, 0, 0)
+        )(params, y_enr, enr_a_mat)
 
-        # Estimate mean of and uncertainty on missing values.
-        x_u_mean, x_u_cov = pcpca_utils.impute_missing_data(params, x_obs, L, M, P, config.feat_dim)
+        # Draw samples from the posterior.
+        rng_x, rng = jax.random.split(rng, 3)
+        x_post_draws = jax.random.multivariate_normal(
+            rng_x, mean=x_mean, cov=x_cov
+        )
 
-        divergence_x = utils.sinkhorn_divergence(x_u_mean[:config.sinkhorn_samples],
-                                                 x_all[:config.sinkhorn_samples, source_index])
-        wandb.log({f'div_x_{source_index+1}': divergence_x}, step=config.n_iter)
+        divergence_x_draws = utils.sinkhorn_divergence(
+            x_post_draws[:config.sinkhorn_samples],
+            x_all[:config.sinkhorn_samples, source_index]
+        )
+        wandb.log(
+            {f'div_post_{source_index+1}': divergence_x_draws},
+            step=config.n_iter
+        )
 
         # Log a figure with new posterior samples.
         if config.log_figure:
-            fig = plotting_utils.show_corner(x_u_mean)._figure
+            fig = plotting_utils.show_corner(x_post_draws)._figure
             wandb.log(
                     {f'posterior samples {source_index+1}': wandb.Image(fig)},
                     step=config.n_iter
                 )
 
         # Plot samples from the prior
-        rng_x, rng_eps_x, rng = jax.random.split(rng, 3)
-        z_x   = jax.random.normal(rng_x, shape=(config.sample_size, config.latent_dim))
-        eps_x = jax.random.multivariate_normal(rng_eps_x,
-                                               mean=jnp.zeros(config.feat_dim),
-                                               cov=jnp.eye(config.feat_dim)*params[1],
-                                               shape=(config.sample_size,))
-        x_u_prior = jnp.matmul(params[0], z_x.T).T + eps_x
+        z_draws = jax.random.normal(
+            rng_x, shape=(config.sample_size, config.latent_dim)
+        )
+        x_prior_draws = jnp.matmul(params['weights'], z_draws[:, :, None])
+        divergence_x_prior_draws = utils.sinkhorn_divergence(
+            x_prior_draws[:config.sinkhorn_samples],
+            x_all[:config.sinkhorn_samples, source_index]
+        )
+        wandb.log(
+            {f'div_prior_{source_index+1}': divergence_x_prior_draws},
+            step=config.n_iter
+        )
 
         if config.log_figure:
-            fig = plotting_utils.show_corner(x_u_prior)._figure
+            fig = plotting_utils.show_corner(x_prior_draws)._figure
             wandb.log(
                     {f'prior samples {source_index+1}': wandb.Image(fig)},
                     step=config.n_iter

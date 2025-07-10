5c72852e878b2520fb137625221a6c5c9b5bb8c0
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2025-07-10 15:19:33 -0400

    modified default parameters in config.

--- ddprism/rand_manifolds/scaling_n_models/config_base_pcpca.py
+++ ddprism/rand_manifolds/scaling_n_models/config_base_pcpca.py
@@ -5,14 +5,14 @@ from ml_collections import ConfigDict
 def get_config():
     """Get the default hyperparameter configuration."""
     config = config_base.get_config()
-    
+
     # Parameters for PCPCA
     config.gamma = [0., 0.3, 0.3]
     config.latent_dim = 5
 
     # Optimization hyperparameters
-    config.n_iter = 4_000
-    config.learning_rate = 1e-2
+    config.n_iter = 100
+    config.learning_rate = 1e-3
     config.lr_schedule = 'linear'
 
     config.log_figure = True
@@ -20,6 +20,6 @@ def get_config():
     # wandb parameters
     config.wandb_kwargs = ConfigDict(
         {'project': 'pcpca_rand_manifolds', 'mode': 'online', 'run_name': None}
-            )
-    
+    )
+
     return config

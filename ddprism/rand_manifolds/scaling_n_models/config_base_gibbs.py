301db5330193e59f5ba0156a0f54a5028d5e32e8
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2025-07-23 14:52:57 -0400

    updated gibbs config.

--- ddprism/rand_manifolds/scaling_n_models/config_base_gibbs.py
+++ ddprism/rand_manifolds/scaling_n_models/config_base_gibbs.py
@@ -5,27 +5,21 @@ import config_base
 def get_config():
     """Get the default hyperparameter configuration."""
     config = config_base.get_config()
-    
-    # Parameters for dataset generation
-    config.n_sources = 2
-    config.alpha = [3.0, 4.0]
-    
-    config.diffusion_em_laps = [32, 128]
-    
+
     # Sampling arguments. Default is to have the number of model evaluations
     # per sampling step be the same for gibbs and joint (therefore fewer
     # steps for gibbs).
     config.sampling_strategy = 'gibbs'
     config.sampling_kwargs = ConfigDict(
         {
-            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 1e-1,
-            'gibbs_rounds': 64
+            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 8e-2,
+            'gibbs_rounds': 64, 'clip_method': 'none'
         }
     )
     config.gaussian_sampling_kwargs = ConfigDict(
         {
-            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 1e-1,
-            'gibbs_rounds': 64
+            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 8e-2,
+            'gibbs_rounds': 64, 'clip_method': 'none'
         }
     )
 

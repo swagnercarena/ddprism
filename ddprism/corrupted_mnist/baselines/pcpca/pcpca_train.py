b88e8846a9aa3cd666d0bf268a6360ee21fc6ee9
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2025-07-24 22:54:30 -0400

    bug fixes.

--- ddprism/corrupted_mnist/baselines/pcpca/pcpca_train.py
+++ ddprism/corrupted_mnist/baselines/pcpca/pcpca_train.py
@@ -171,9 +171,10 @@ def run_pcpca(config_pcpca, workdir):
     y_bkg = y_bkg_all[:full_res_samples]
 
     # Get mnist classifier model and params.
+    rng_class, rng = jax.random.split(rng)
     mnist_model, mnist_params = image_metrics.get_model(
         FLAGS.mnist_classifier_path, rng_class,
-        **config.get('mnist_classifier_kwargs', {})
+        **config_mnist.get('mnist_classifier_kwargs', {})
     )
 
     # Normalize the datasets, and subtract the mean of the background dataset

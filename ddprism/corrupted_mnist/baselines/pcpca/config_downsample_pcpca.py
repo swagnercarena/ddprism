78d798dfd7e8fb628618b2019718f958bcdd131d
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2025-07-24 21:42:46 -0400

    configuration for downsampled pcpca.

---
+++ ddprism/corrupted_mnist/baselines/pcpca/config_downsample_pcpca.py
@@ -0,0 +1,24 @@
+"""Basic configuration for PCPCA with gradient descent."""
+from ml_collections import ConfigDict
+from ddprism.corrupted_mnist import config_downsample_grass
+from ddprism.corrupted_mnist import config_downsample_mnist
+
+
+def get_config():
+    """Get the default hyperparameter configuration."""
+    config = ConfigDict()
+
+    # Configuration files for grass and MNIST digits
+    config.config_grass = config_downsample_grass.get_config()
+    config.config_mnist = config_downsample_mnist.get_config()
+
+    # PCPCA parameters
+    config.gamma = 0.01
+    config.latent_dim = 8
+
+    # Wandb parameters
+    config.wandb_kwargs = ConfigDict(
+        {'project': 'pcpca-mnist-down', 'mode': 'online', 'run_name': None}
+    )
+
+    return config

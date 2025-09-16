fe3309983ab099652eee01aeb58a25c61df9471f
Sebastian Wagner-Carena <swagnercarena@gmail.com>
2025-09-16 15:26:51 +0200

    updated scaling_n_models configs.

--- ddprism/rand_manifolds/scaling_n_models/config_base_gibbs.py
+++ ddprism/rand_manifolds/scaling_n_models/config_base_gibbs.py
@@ -6,6 +6,7 @@ from ml_collections import ConfigDict
 def get_config():
     """Get the default hyperparameter configuration."""
     config = config_base.get_config()
+    config.diffusion_em_laps = [16, 96, 192]
 
     # Sampling arguments. Default is to have the number of model evaluations
     # per sampling step be the same for gibbs and joint (therefore fewer
@@ -13,13 +14,13 @@ def get_config():
     config.sampling_strategy = 'gibbs'
     config.sampling_kwargs = ConfigDict(
         {
-            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 8e-2,
+            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 1e-1,
             'gibbs_rounds': 64, 'clip_method': 'none'
         }
     )
     config.gaussian_sampling_kwargs = ConfigDict(
         {
-            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 8e-2,
+            'steps': 256, 'sampler': 'pc', 'corrections': 1, 'tau': 1e-1,
             'gibbs_rounds': 64, 'clip_method': 'none'
         }
     )
